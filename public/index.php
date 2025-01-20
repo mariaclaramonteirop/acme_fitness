b639c180c6a2e1a1f5c8c70802149e883884c779
Maria Clara Monteiro <mcmpacheco7@gmail.com>
2025-01-20 06:30:54 -0300

    Fix: ajusta index.php

--- public/index.php
+++ public/index.php
@@ -4,9 +4,23 @@ use Slim\Factory\AppFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$container = 
+$container = require __DIR__ . '/../src/dependencies.php';
+AppFactory::setContainer($container);
+
+
 $app = AppFactory::create();
 
-$routes = require __DIR__ . '/../src/routes.php';
+
+
+$routes = require __DIR__ . '/../src/routes/routes.php';
+$routes($app);
+
+$app->add(function ($request, $handler) {
+    $routes = $this->getRouteCollector()->getRoutes();
+    foreach ($routes as $route) {
+        echo $route->getPattern() . PHP_EOL;
+    }
+    return $handler->handle($request);
+});
 
 $app->run();
