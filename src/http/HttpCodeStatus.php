c3f4832c542603ae26efb179664edf8bb7225c76
Maria Clara Monteiro <mcmpacheco7@gmail.com>
2025-01-22 06:31:25 -0300

    Cleanup: removendo constantes não utilizadas

--- src/http/HttpCodeStatus.php
+++ src/http/HttpCodeStatus.php
@@ -5,22 +5,7 @@ namespace http;
 abstract class HttpCodeStatus {
     const OK = 200;
     const CREATED = 201;
-    const ACCEPTED = 202;
-    const NO_CONTENT = 204;
-
-    const MOVED_PERMANENTLY = 301;
-    const FOUND = 302;
-    const NOT_MODIFIED = 304;
-
     const BAD_REQUEST = 400;
-    const UNAUTHORIZED = 401;
-    const FORBIDDEN = 403;
     const NOT_FOUND = 404;
-    const METHOD_NOT_ALLOWED = 405;
-
     const INTERNAL_SERVER_ERROR = 500;
-    const NOT_IMPLEMENTED = 501;
-    const BAD_GATEWAY = 502;
-    const SERVICE_UNAVAILABLE = 503;
-    const GATEWAY_TIMEOUT = 504;
 }
